1b116472cbfc84b9e6fcb8f415d0fd5917ecff57
Bryanctllc <bryan.coutellec@hotmail.fr>
2021-06-29 17:33:10 +0200

    imgs

--- src/components/Apport.vue
+++ src/components/Apport.vue
@@ -19,47 +19,47 @@
       <div class="columns is-justify-content-space-evenly mt-5">
         <div class="column is-3 has-text-centered">
           <div class="column image bigrounded">
-            <img class="rounded" src="../assets/imgrounded1.jpg" />
+            <img class="rounded" src="../assets/Producteur.jpeg" alt="Image agriculteur"/>
           </div>
           <p class="is-size-3 has-text-weight-bold my-5">
             Un producteur :
           </p>
-          <p class="is-size-5">
+          <p class="is-size-5 has-text-justified">
             Besoin de trouver de nouvelles opportunitées pour vendre vos
             produits ? Nous sommes là pour vous aider à vendre vos productions
             au plus près de chez vous. Un accès direct aux étals de marché de
             votre région.
           </p>
-          <a class="button my-6 Neumorphism is-size-4" href="#"><b>En savoir +</b></a>
+          <a class="button my-6 Neumorphism is-size-4" href="#">En savoir +</a>
         </div>
         <div class="column is-3 has-text-centered">
           <div class="column image bigrounded">
-            <img class="rounded" src="../assets/imgrounded2.jpg" />
+            <img class="rounded" src="../assets/Mairie.jpeg" alt="image d'une Mairie"/>
           </div>
           <p class="is-size-3 has-text-weight-bold my-5">
             Une mairie :
           </p>
-          <p class="is-size-5">
+          <p class="is-size-5 has-text-justified">
             Marre de ne trouver que des tomates d’espagne sur vos marchés ?
             Proposez les produits locaux à vos concitoyens et remplissez les
             étals de vos marchés des délices de nos régions.
           </p>
-          <a class="button my-6 Neumorphism is-size-4" href="#"><b>En savoir +</b></a>
+          <a class="button my-6 Neumorphism is-size-4" href="#">En savoir +</a>
         </div>
         <div class="column is-3 has-text-centered">
           <div class="column image bigrounded">
-            <img class="rounded" src="../assets/imgrounded3.jpg" />
+            <img class="rounded" src="../assets/Consommateur.jpeg" alt="Image de personnes échangeant dans un marché"/>
           </div>
           <p class="is-size-3 has-text-weight-bold my-5">
             Un consommateur :
           </p>
-          <p class="is-size-5">
+          <p class="is-size-5 has-text-justified">
             Une envie de découvrir les ressources locales des agriculteurs
             environnant ? Les prix des produits locaux vous freine dans vos
             achats ? Grace à nous, accumulez des points et profitez de réduction
             sur vos marchés locaux.
           </p>
-          <a class="button my-6 Neumorphism is-size-4" href=""><b>En savoir +</b></a>
+          <a class="button my-6 Neumorphism is-size-4" href="">En savoir +</a>
         </div>
       </div>
     </div>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,37 @@
 <template>
   <div class="columns is-centered is-vcentered">
     <div class="column is-5">
-      <p class="is-size-1 is-uppercase has-text-weight-bold mb-5">Les meilleurs près de chez vous !</p>
-      <p class="is-size-4">Une envie de consommer des produits locaux ? La halte mitoyenne fait le lien entre les champs d’à coté et votre assiette.</p>
+      <p class="is-size-1 is-uppercase has-text-weight-bold mb-5">
+        Les meilleurs près de chez vous !
+      </p>
+      <p class="is-size-4">
+        Une envie de consommer des produits locaux ? La halte mitoyenne fait le
+        lien entre les champs d’à coté et votre assiette.
+      </p>
       <p class="is-size-4 mt-5">Mangez français, mangez mieux !</p>
-      <a class="button mt-6 Neumorphism is-size-3" href="https://www.instagram.com/la_halte_mitoyenne/" target="_blank"><b>Nous suivre sur les réseaux</b></a>
+      <a
+        class="button mt-6 Neumorphism is-size-3"
+        href="https://www.instagram.com/la_halte_mitoyenne/"
+        target="_blank"
+        ><b>Nous suivre sur les réseaux</b></a
+      >
     </div>
-    <div class="column is-5"><img id="roundedimg" src="../assets/img1.jpg" alt=""></div>
+    <div class="column is-5">
+      <figure class="image is-square">
+        <img id="roundedimg" src="../assets/Marche.jpeg" alt="" />
+      </figure>
+
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -25,16 +40,15 @@ a {
   color: black;
 }
 
-#roundedimg{
+#roundedimg {
   border-radius: 25px;
 }
 
-.Neumorphism{
-  background: #F0F0F3;
+.Neumorphism {
+  background: #f0f0f3;
   border: 5px solid rgba(255, 255, 255, 0.3);
   box-sizing: border-box;
-  box-shadow: -10px -10px 30px #FFFFFF, 10px 10px 20px #AEAEC0;
+  box-shadow: -10px -10px 30px #ffffff, 10px 10px 20px #aeaec0;
   border-radius: 98px;
 }
-
 </style>
